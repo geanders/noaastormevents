0816d40f7b5c0eb4423370a4eceec3cc4d5d41f7
Ziyu Chen <zailchen17@icloud,com>
2016-06-09 01:43:27 -0600

    Add combined storm data set from 1996-2016; Remove find_file_name function from each function.

--- R/map_indirectinjuries.R
+++ R/map_indirectinjuries.R
@@ -1,31 +1,3 @@
-#' Find the file name for a specific year
-#'
-#' This function will find the name of the detailed file from Storm Events Database
-#' for a specific year.
-#'
-#' @param year A character string giving the year.
-#'
-#'
-#' @examples
-#' find_file_name(year = 1999)
-#'
-#' find_file_name(year = 2003)
-#'
-#' @export
-#'
-find_file_name <- function(year = NULL) {
-  page <-  XML::readHTMLTable("http://www1.ncdc.noaa.gov/pub/data/swdi/stormevents/csvfiles/")
-  file_name <- page[[1]]$Name
-  year <- year
-  file_type <- "details"
-  file_year <- paste0("_d",year,"_")
-  file.name <- grep(file_type, grep(file_year, file_name, value = TRUE), value = TRUE)
-
-
-  return(file.name)
-}
-
-
 #' Find all indirect injury listings for date range
 #'
 #' This function will find all of the indirect injuries in the US for a specified date
