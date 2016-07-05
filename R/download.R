60e2da0c00655f524ea347d4d9afba5eaa8132be
Brooke Anderson <geanderson@jhsph.edu>
2016-07-04 20:52:23 -0600

    Add documentation for `download_file`

--- R/download.R
+++ R/download.R
@@ -1,6 +1,21 @@
-download_file <- function(Year = NULL){
-  file_name <- find_file_name(Year)
-  path_name <- paste0("http://www1.ncdc.noaa.gov/pub/data/swdi/stormevents/csvfiles/",file_name)
+#' Download storm data file for a given year
+#'
+#' This function takes a year for which you want to download storm data,
+#' checks to see if it's already been downloaded and cached, and, if not,
+#' downloads and caches it from the NOAA's online storm events files.
+#'
+#' @inheritParams find_file_name
+#'
+#' @note
+#' This function caches downloaded storm data into an object called \code{lst}
+#' that persist throughout the R session but is deleted at the end of the R
+#' session (as long as the R history is not saved at the end of the session).
+#' This saves time if the user uses the storm data from the same year for
+#' several commands.
+download_file <- function(year, file_type = "detail"){
+  file_name <- find_file_name(year = year, file_type = file_type)
+  path_name <- paste0("http://www1.ncdc.noaa.gov/pub/data/swdi/stormevents/",
+                      "csvfiles/",file_name)
 
   if(!exists("lst")) {
     temp <- tempfile()
@@ -18,6 +33,7 @@ download_file <- function(Year = NULL){
 }
 
 get_file <- function(first_date = NULL, last_date = NULL, storm = NULL) {
+
   if(!is.null(first_date) & !is.null(last_date)){
     first_date <- lubridate::ymd(first_date)
     last_date <- lubridate::ymd(last_date)
@@ -26,10 +42,9 @@ get_file <- function(first_date = NULL, last_date = NULL, storm = NULL) {
     }
   }
 
-
   if(!is.null(storm)){
-    Year <- gsub("[^0-9]", "", storm)
-    download_file(Year = Year)
+    storm_year <- gsub("[^0-9]", "", storm)
+    download_file(Year = storm_year)
     data <- lst[[as.character(Year)]]
   } else {
     if(lubridate::year(first_date) == lubridate::year(last_date)) {
