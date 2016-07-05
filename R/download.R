708ced867d45a50c1d799907ab7a7d3811f4fb5f
Brooke Anderson <geanderson@jhsph.edu>
2016-07-04 20:56:18 -0600

    Change `download_file` name to `download_storm_data`.
    This function name is less generic, so less likely to interfere with functions from other packages.

--- R/download.R
+++ R/download.R
@@ -12,7 +12,7 @@
 #' session (as long as the R history is not saved at the end of the session).
 #' This saves time if the user uses the storm data from the same year for
 #' several commands.
-download_file <- function(year, file_type = "detail"){
+download_storm_data <- function(year, file_type = "detail"){
   file_name <- find_file_name(year = year, file_type = file_type)
   path_name <- paste0("http://www1.ncdc.noaa.gov/pub/data/swdi/stormevents/",
                       "csvfiles/",file_name)
@@ -44,19 +44,19 @@ get_file <- function(first_date = NULL, last_date = NULL, storm = NULL) {
 
   if(!is.null(storm)){
     storm_year <- gsub("[^0-9]", "", storm)
-    download_file(Year = storm_year)
+    download_storm_data(Year = storm_year)
     data <- lst[[as.character(Year)]]
   } else {
     if(lubridate::year(first_date) == lubridate::year(last_date)) {
     Year <- lubridate::year(first_date)
-    download_file(Year = Year)
+    download_storm_data(Year = Year)
     data <- lst[[as.character(Year)]]
     } else {
       data <- NULL
       for (Year in
            as.numeric(as.character(lubridate::year(first_date))):
            as.numeric(as.character(lubridate::year(last_date)))) {
-      download_file(Year = Year)
+      download_storm_data(Year = Year)
       data <- rbind(data, lst[[as.character(Year)]])
       }
     }
