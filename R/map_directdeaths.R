d072fe97f20d6984075e763946927a96d1dca9cf
Ziyu Chen <zailchen17@icloud,com>
2016-09-07 02:02:09 -0600

    Fix the "CZ_TYPE" issue. The plotting functions are now able to give better/smarter labels based on the amount of data points included.

--- R/map_directdeaths.R
+++ R/map_directdeaths.R
@@ -26,8 +26,8 @@ find_direct_deaths <- function(date_range = NULL, ts_only = FALSE,
   storm <- processed_inputs$storm
 
   storm_data <- create_storm_data(date_range = date_range,  storm = storm) %>%
-    dplyr::select(BEGIN_YEARMONTH, BEGIN_DAY, END_YEARMONTH, END_DAY,
-                  STATE_FIPS, CZ_FIPS, CZ_TYPE, EVENT_TYPE, DEATHS_DIRECT) %>%
+    dplyr::select(BEGIN_YEARMONTH, BEGIN_DAY, END_YEARMONTH, END_DAY, STATE,
+                  CZ_NAME, EVENT_TYPE, DEATHS_DIRECT) %>%
     dplyr::rename(type = EVENT_TYPE,
                   direct_deaths = DEATHS_DIRECT) %>%
     adjust_storm_data(date_range = date_range, ts_only = ts_only,
@@ -80,18 +80,21 @@ map_direct_deaths <- function(date_range = NULL, ts_only = FALSE, east_only = TR
     dplyr::full_join(county.regions, by = "region") %>%
     dplyr::filter(!is.na(county.name))
 
-  if(east_only){
+if(east_only){
     map_data <- dplyr::filter(map_data, state.name %in% eastern_states)
   }
 
   map_data <- map_data %>% dplyr::select(region, value)
   map_data$value <- as.numeric(as.character(map_data$value))
 
-  map_data <- map_data %>% dplyr::group_by(region)
-  map_data <- dplyr::summarise(map_data, value = sum(value, na.rm = TRUE))
-  map_data <-  dplyr::ungroup(map_data)
+  map_data <- map_data %>%
+    dplyr::group_by(region) %>%
+    dplyr::summarise(value = sum(value, na.rm = TRUE)) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(value = factor(value, levels = 0:max(value)))
 
-  max_value <- max(map_data$value)
+if(length(unique(map_data$value)) > 9) {
+  max_value <- max(as.numeric(as.character(map_data$value)))
   ceil <- floor(max_value/10^(floor(log10(max_value))))*10^(floor(log10(max_value)))
   decimal <- floor(max_value/10^(floor(log10(max_value))))
 
@@ -118,20 +121,28 @@ map_direct_deaths <- function(date_range = NULL, ts_only = FALSE, east_only = TR
   palette_name <- "Reds"
   map_palette <- RColorBrewer::brewer.pal(length(breaks)  , name = palette_name)
 
-  if(max(map_data$value) > max(breaks)){
-    breaks <- c(breaks, max(map_data$value))
+  if(max_value > max(breaks)){
+    breaks <- c(breaks, max_value)
   }
 
   map_palette[1] <- "#ffffff"
   map_data <- map_data %>%
-    dplyr::mutate_(value = ~ cut(value, breaks = breaks,
+    dplyr::mutate_(value = ~ cut(as.numeric(as.character(value)), breaks = breaks,
                                  include.lowest = TRUE, right = F))
   level_names <- levels(map_data$value)
   level_names[length(level_names)] <- paste0(">=", ceil)
+} else {
+  map_palette <- RColorBrewer::brewer.pal(length(unique(map_data$value)), name = "Reds")
+  map_palette[1] <- "#ffffff"
+  level_names <- levels(map_data$value)
+}
+
   map_data$value <- factor(map_data$value,
                            levels = levels(map_data$value),
                            labels = level_names)
   out <- choroplethr::CountyChoropleth$new(map_data)
+  out$ggplot_scale <- ggplot2::scale_fill_manual(name = "# of direct deaths",
+                                                 values = map_palette)
 
 
   if(east_only){
@@ -141,11 +152,6 @@ map_direct_deaths <- function(date_range = NULL, ts_only = FALSE, east_only = TR
     continental_states <- all_states[!(all_states %in% c("alaska", "hawaii"))]
     out$set_zoom(continental_states)
   }
-
-
-  out$ggplot_scale <- ggplot2::scale_fill_manual(name = "# of direct deaths",
-                                                 values = map_palette)
-
 
   if(add_tracks){
     tracks_map <- hurricaneexposure::map_tracks(storms = storm,
--- R/map_indirectdeaths.R
+++ R/map_indirectdeaths.R
@@ -24,8 +24,8 @@ find_indirect_deaths <- function(date_range = NULL, ts_only = FALSE,
   storm <- processed_inputs$storm
 
   storm_data <- create_storm_data(date_range = date_range,  storm = storm) %>%
-    dplyr::select(BEGIN_YEARMONTH, BEGIN_DAY, END_YEARMONTH, END_DAY,
-                  STATE_FIPS, CZ_FIPS, CZ_TYPE, EVENT_TYPE, DEATHS_INDIRECT) %>%
+    dplyr::select(BEGIN_YEARMONTH, BEGIN_DAY, END_YEARMONTH, END_DAY, STATE,
+                  CZ_NAME, EVENT_TYPE, DEATHS_INDIRECT) %>%
     dplyr::rename(type = EVENT_TYPE,
                   indirect_deaths = DEATHS_INDIRECT) %>%
     adjust_storm_data(date_range = date_range, ts_only = ts_only,
@@ -82,51 +82,62 @@ map_indirect_deaths <- function(date_range = NULL, ts_only = FALSE, east_only = 
   map_data <- map_data %>% dplyr::select(region, value)
   map_data$value <- as.numeric(as.character(map_data$value))
 
-  map_data <- map_data %>% dplyr::group_by(region)
-  map_data <- dplyr::summarise(map_data, value = sum(value, na.rm = TRUE))
-  map_data <-  dplyr::ungroup(map_data)
-
-  max_value <- max(map_data$value)
-  ceil <- floor(max_value/10^(floor(log10(max_value))))*10^(floor(log10(max_value)))
-  decimal <- floor(max_value/10^(floor(log10(max_value))))
-
-  if(decimal == 1) {
-    breaks <- c(0,seq(1, ceil+1, by = ceil/5))
-  } else if(decimal == 2) {
-    breaks <- c(0,seq(1, ceil+1, by = ceil/5))
-  } else if(decimal == 3) {
-    breaks <- c(0,seq(1, ceil+1, by = ceil/6))
-  } else if(decimal == 4) {
-    breaks <- c(0,seq(1, ceil+1, by = ceil/5))
-  } else if(decimal == 5) {
-    breaks <- c(0,seq(1, ceil+1, by = ceil/5))
-  } else if(decimal == 6) {
-    breaks <- c(0,seq(1, ceil+1, by = ceil/6))
-  } else if(decimal == 7) {
-    breaks <- c(0,seq(1, ceil+1, by = ceil/7))
-  } else if(decimal == 8) {
-    breaks <- c(0,seq(1, ceil+1, by = ceil/5))
-  } else if(decimal == 9) {
-    breaks <- c(0,seq(1, ceil+1, by = ceil/5))
-  }
-
-  palette_name <- "Reds"
-  map_palette <- RColorBrewer::brewer.pal(length(breaks)  , name = palette_name)
-
-  if(max(map_data$value) > max(breaks)){
-    breaks <- c(breaks, max(map_data$value))
-  }
-
-  map_palette[1] <- "#ffffff"
   map_data <- map_data %>%
-    dplyr::mutate_(value = ~ cut(value, breaks = breaks,
-                                 include.lowest = TRUE, right = F))
-  level_names <- levels(map_data$value)
-  level_names[length(level_names)] <- paste0(">=", ceil)
+    dplyr::group_by(region) %>%
+    dplyr::summarise(value = sum(value, na.rm = TRUE)) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(value = factor(value, levels = 0:max(value)))
+
+  if(length(unique(map_data$value)) > 9) {
+    max_value <- max(as.numeric(as.character(map_data$value)))
+    ceil <- floor(max_value/10^(floor(log10(max_value))))*10^(floor(log10(max_value)))
+    decimal <- floor(max_value/10^(floor(log10(max_value))))
+
+    if(decimal == 1) {
+      breaks <- c(0,seq(1, ceil+1, by = ceil/5))
+    } else if(decimal == 2) {
+      breaks <- c(0,seq(1, ceil+1, by = ceil/5))
+    } else if(decimal == 3) {
+      breaks <- c(0,seq(1, ceil+1, by = ceil/6))
+    } else if(decimal == 4) {
+      breaks <- c(0,seq(1, ceil+1, by = ceil/5))
+    } else if(decimal == 5) {
+      breaks <- c(0,seq(1, ceil+1, by = ceil/5))
+    } else if(decimal == 6) {
+      breaks <- c(0,seq(1, ceil+1, by = ceil/6))
+    } else if(decimal == 7) {
+      breaks <- c(0,seq(1, ceil+1, by = ceil/7))
+    } else if(decimal == 8) {
+      breaks <- c(0,seq(1, ceil+1, by = ceil/5))
+    } else if(decimal == 9) {
+      breaks <- c(0,seq(1, ceil+1, by = ceil/5))
+    }
+
+    palette_name <- "Reds"
+    map_palette <- RColorBrewer::brewer.pal(length(breaks)  , name = palette_name)
+
+    if(max_value > max(breaks)){
+      breaks <- c(breaks, max_value)
+    }
+
+    map_palette[1] <- "#ffffff"
+    map_data <- map_data %>%
+      dplyr::mutate_(value = ~ cut(as.numeric(as.character(value)), breaks = breaks,
+                                   include.lowest = TRUE, right = F))
+    level_names <- levels(map_data$value)
+    level_names[length(level_names)] <- paste0(">=", ceil)
+  } else {
+    map_palette <- RColorBrewer::brewer.pal(length(unique(map_data$value)), name = "Reds")
+    map_palette[1] <- "#ffffff"
+    level_names <- levels(map_data$value)
+  }
+
   map_data$value <- factor(map_data$value,
                            levels = levels(map_data$value),
                            labels = level_names)
   out <- choroplethr::CountyChoropleth$new(map_data)
+  out$ggplot_scale <- ggplot2::scale_fill_manual(name = "# of indirect deaths",
+                                                 values = map_palette)
 
   if(east_only){
     out$set_zoom(eastern_states)
@@ -135,10 +146,6 @@ map_indirect_deaths <- function(date_range = NULL, ts_only = FALSE, east_only = 
     continental_states <- all_states[!(all_states %in% c("alaska", "hawaii"))]
     out$set_zoom(continental_states)
   }
-
-
-  out$ggplot_scale <- ggplot2::scale_fill_manual(name = "# of indirect deaths",
-                                                 values = map_palette)
 
 
   if(add_tracks){
