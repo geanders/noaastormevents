2dd3699ee15b1c0eaf742eb19e8066fecefd36dd
Therese Kondash <theresekondash@gmail.com>
2020-07-09 15:04:00 -0600

    new tests for find file name and map events

--- tests/testthat/test-findfilename.R
+++ tests/testthat/test-findfilename.R
@@ -1,12 +1,12 @@
 context("finding filename from NOAA webpage")
 
-# test_that("Can find filename for 1999", {
-#  skip_on_cran()
-#  expect_equal(length(find_file_name(year = 1999)), 1)
-#})
 
 test_that("Can find filename for 1999", {
   skip_on_cran()
-  expect_silent(find_file_name(year = 1999))
+  expect_silent(find_file_name(year = 1999, file_type = "fatalities"))
   })
 
+test_that("Only one file name returned for 1999", {
+  skip_on_cran()
+  expect_equal(length(find_file_name(year = 1999)), 1)
+})
